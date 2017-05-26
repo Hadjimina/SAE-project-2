1da9033caabdd329e0a473ce544c57c53f0d8b98
David-Flurin <davidn@student.ethz.ch>
2017-05-26 16:02:07 +0100

    beep boop

--- README.md
+++
@@ -1,4 +0,0 @@
-# SAE-project-2
-#### I am in deep pain. Please help me
-![alt text](rick.jpg "I am in deep pain.")
-
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-base=$(pwd)
-export apron_home=/home/sae/apron/japron
-export CLASSPATH=.:$base/soot-2.5.0.jar:$apron_home/apron.jar:$apron_home/gmp.jar:$base/bin
-export LD_LIBRARY_PATH=$apron_home/:/usr/local/lib
-
-java ch.ethz.sae.Verifier "$1"
